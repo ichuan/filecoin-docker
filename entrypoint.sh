349a5f6f558d1b70c12356a29793fe2af4c17157
ichuan <iyanchuan@gmail.com>
2020-10-15 18:23:28 +0800

    fix: use nc to wait api

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,11 @@ if test $# -eq 0; then
   echo -e "[API]\nListenAddress=\"/ip4/0.0.0.0/tcp/1234/http\"" > $LOTUS_CONFIG
   echo -e "[Libp2p]\n[Pubsub]\n[Client]\n[Metrics]" >> $LOTUS_CONFIG
   /opt/coin/lotus daemon --config $LOTUS_CONFIG &
-  /opt/coin/lotus wait-api
+  # /opt/coin/lotus wait-api
+  echo 'Waiting for lotus JSON RPC...'
+  while ! nc -z -w 1 127.0.0.1 1234; do
+    sleep 1
+  done
   # generate filecoin-service.toml
   JWT=`cat /root/.lotus/token`
   echo -e "[service]\naddress=\"0.0.0.0:3030\"" > $SERVICE_CONFIG
